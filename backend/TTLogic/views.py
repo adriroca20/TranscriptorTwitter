091e9b133fa8b45945e9ef98d82d0fae22b25e38
Microhard1 <microhardquiz@gmail.com>
2022-12-22 20:01:15 +0100

    Creado transciptor a PDF

--- backend/TTLogic/views.py
+++ backend/TTLogic/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .extractor import Extractor
+
 # Create your views here.
 
 def transcript(request):
+    # url="https://twitter.com/i/spaces/1gqxvyzbdMRJB?s=20"
     extractor = Extractor()
-    url = request.GET.get("url")
-    extractor.getCaption(url)
-    return HttpResponse(200)
+    url = request.GET.get("url", "")
+    buffer = extractor.getCaption(url)
+    return FileResponse(buffer, as_attachment=True, filename="transcriptionTWT.pdf")
